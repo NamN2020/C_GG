8cd5102e75e94b3b1e728e3ba10a8413ae423a48
Nam <NamV9321@gmail.com>
2024-02-08 22:29:26 -0800

    EOD

--- Day2/pointer.c
+++ Day2/pointer.c
@@ -1,13 +1,6 @@
 // 2/8/24 
 /*
     Today covered sections worth noting: 
-    
-    First Git PUSH
-
-    PUsh
-    PusH
-
-    Do i get another bug?
 */
 #include <stdio.h>
 
@@ -15,5 +8,19 @@ int main()
 {
     // %zu is the format specifier for type size_t
     printf("an int uses %zu bytes of memory\n", sizeof(int));
+
+    int i = 10;
+    printf("The value of i is %d\n", i);
+    printf("And its address is %p\n", (void *)&i);
+    // %p expects the argument to be a pointer to void
+    // so we cast it to make the compiler happy.
+    int *p; // p's type is "pointer to an int", or "int-pointer"
+    p = &i;  // p now points to i, p holds address of i
+
+    i = 10;  // i is now 10
+    *p = 20; // the thing p points to (namely i!) is now 20!!
+
+    printf("i is %d\n", i);   // prints "20"
+    printf("i is %d\n", *p);  // "20"! dereference-p is the same as i!
     return 0;
 }
