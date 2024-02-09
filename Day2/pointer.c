af168971004ee7c58acc05287e89c23532dc8d7a
NamN2020 <NamV9321@gmail.com>
2024-02-08 21:43:50 -0800

    Create pointer.c

---
+++ Day2/pointer.c
@@ -0,0 +1,13 @@
+// 2/8/24 
+/*
+    Today covered sections worth noting: 
+        j
+*/
+#include <stdio.h>
+
+int main()
+{
+    // %zu is the format specifier for type size_t
+    printf("an int uses %zu bytes of memory\n", sizeof(int));
+    return 0;
+}
