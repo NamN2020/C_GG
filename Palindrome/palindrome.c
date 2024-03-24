00f3d2783428f905633e4d67832d65063de48c87
NamN2020 <NamV9321@gmail.com>
2024-03-24 13:43:09 -0700

    Finished Palindrome

--- Palindrome/palindrome.c
+++ Palindrome/palindrome.c
@@ -2,52 +2,116 @@
 #include<stdlib.h>
 #include<ctype.h>
 #include<string.h>
+#include<stdbool.h>
 
-char* strLength(char* str);
+int strLength(char* str);
+bool isPalindrome(char* str);
 void replace_char(char* str, char oldChar, char newChar);
+bool leetCodeSol(char* str);
+int lowerCaseConvert(char c);
+bool isAlphaNumeric(char c);
 
 int main()
 {   
     char test1[] = "A man, a plan, a canal: Panama";    
-    char test2[] = "         r  a c   e  car";
+    char test2[] = "R   a c   e    car";
     
     int num = 10;
     int *pointer = &num;
 
     //printf("This is num: %d\nThis is size of int pointer: %zu\nThis is the pointer: %p\n", num, sizeof(pointer),pointer);
     //printf("Test1: %s\n", test1);
-    strLength(test2);
+    //strLength(test2);
+    //isPalindrome(test2);
     printf("strLength Test: %s\n", test2); 
-    
+
+    if(leetCodeSol(test1))
+        printf("%s\n", "IS");
+    else
+        printf("%s\n", "IS NOT");
+
     return 0;
 }
 
-char* strLength(char* str)
-{
+
+bool isPalindrome(char* str)
+{ 
+    char* temp = malloc((strlen(str))*(sizeof(char)));
     char* result = str;  
+    int alphaLength = strLength(str);
+    int i = 0;
     do
     {
-        //printf("%s","IN WHILE");
-        if(isalpha(*str))
+        if(isalpha(*str) && i < alphaLength)
         {   
-            *result++ = *str;
-            printf("%s\n", "IN");           
+            if (*str > 64 && *str < 91)
+                *str = *str + 32;        
+
+            temp[i] = *str;
+            i++;
         }
         else
             *str = ' ';
         str++;
+        i++;
     } while (*str != '\0');
-    str = result;
+
+    printf("Temp: %s\n", temp);
+    return(!(strcmp(str, temp)));
 }
 
-void replace_char(char* str, char oldChar, char newChar) {
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] == oldChar) {
-            str[i] = newChar;
+
+int strLength(char* str)
+{
+    char* tmp = str;
+    int i = 0;
+    do
+    {
+        i++;
+        tmp++;
+    } while (*tmp != '\0');
+    return i;
+}
+
+bool leetCodeSol(char* str) {
+    int start = 0;
+    int last = strlen(str) - 1;
+
+    printf("String Length: %d\n", last);
+    while(start < last)
+    {
+        if (isAlphaNumeric(str[start]))
+        {
+            if (isAlphaNumeric(str[last]))
+            {
+                if (lowerCaseConvert(str[start]) != lowerCaseConvert(str[last]))
+                    return false;
+                start++;
+                last--;
+            }
+            else
+            {
+                last--;
+            }
+        }
+        else
+        {
+            start++;
         }
     }
+
+    return true;
 }
 
+int lowerCaseConvert(char c)
+{
+    return (c >= 'A' && c <= 'Z') ? c + 32 : c;
+}
+
+bool isAlphaNumeric(char c)
+{
+    return ((c >= 'A'  && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= 48 && c <= 57));
+}
 
 // Laptop commit
 
