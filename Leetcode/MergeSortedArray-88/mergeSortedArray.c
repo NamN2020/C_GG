45cc07b24a1b84db2f86e307f86efc70c547fda6
NamN2020 <namv9321@gmail.com>
2024-03-25 01:16:04 -0700

    progres

--- Leetcode/MergeSortedArray-88/mergeSortedArray.c
+++ Leetcode/MergeSortedArray-88/mergeSortedArray.c
@@ -1,7 +1,38 @@
-#include<stdio.h>
+#include <stdio.h>
+
+void merge(int *nums1, int nums1Size, int m, int *nums2, int nums2Size, int n);
 
 int main()
 {
+    int nums1[] = {1, 2, 3, 0, 0, 0};
+    int nums2[] = {2, 5, 6};
+    int nums1Size = sizeof(nums1) / sizeof(int);
+    int nums2Size = sizeof(nums2) / sizeof(int);
+
+    printf("nums1Size: %d\n", nums1Size);
+    printf("nums2Size: %d\n", nums2Size);
+
+    merge(nums1, nums1Size, 3, nums2, nums2Size, 3);
 
     return 0;
+}
+
+void merge(int *nums1, int nums1Size, int m, int *nums2, int nums2Size, int n)
+{
+    if (m > nums1Size || n > nums2Size)
+        return;
+
+    int i = 0;
+    int j = 0;
+
+    do
+    {
+        if(nums2[j] >= nums1[i])
+        {
+            nums1[i+1] = nums1[i];
+            nums1[i] = nums1[j];
+            i++;
+            j++;
+        }
+    } while (i < m && j < n);
 }
